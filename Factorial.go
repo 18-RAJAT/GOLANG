14873bffbcc7b704f720d2b510c482daac418ae1
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-10-12 02:10:26 +0530

    factorial

---
+++ Factorial.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+    var nFact,fact int
+    fact=1
+
+    fmt.Println("Enter the number ")
+    fmt.Scanln(&nFact)
+
+    for i := 1; i <= nFact;i++{
+        fact=fact*i;
+    }
+    fmt.Println("factorial is",nFact," =",fact)
+}
