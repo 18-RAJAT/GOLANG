156a21006c270e855d7f717f86cd532103a1a81f
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-10-19 12:52:24 +0530

    Fibonacci

--- Fibonacci.go
+++ Fibonacci.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func fib(n int) {
+	var n3, n1, n2 int = 0, 0, 1
+	for i := 1; i <= n; i++ {
+
+		fmt.Printf(" %d ", n1)
+		n3 = n1 + n2
+		n1 = n2
+		n2 = n3
+	}
+}
+
+func main() {
+	fib(10)
+}
